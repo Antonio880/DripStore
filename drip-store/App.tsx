7c0a58e0170dd0f8b6b78d76ff74040cf548f648
gabrielfel1x <gabrielfelxx@gmail.com>
2024-08-10 13:48:30 -0300

    feat: finally emphasis collection session

--- drip-store/App.tsx
+++ drip-store/App.tsx
@@ -1,24 +1,12 @@
-
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import HomePage from "./src/pages/HomePage";
 import ProductPage from "./src/pages/ProductPage";
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/produtos" element={<ProductPage />} />
-    </Routes>
-  );
-import React from 'react';
-import { useRoutes } from 'react-router-dom';
-import HomePage from './src/pages/HomePage';
-import ProductPage from './src/pages/ProductPage';
-import DetailsPage from './src/pages/DetailsPage';
+import DetailsPage from "./src/pages/DetailsPage";
 
 const AppRoutes = () => {
   const routes = useRoutes([
+    { path: "/", element: <HomePage /> },
     { path: "/home", element: <HomePage /> },
     { path: "/produtos", element: <ProductPage /> },
     { path: "/produtos/:id", element: <DetailsPage /> },
@@ -27,6 +15,6 @@ const AppRoutes = () => {
   return routes;
 };
 
-export default function App(){
-    return <AppRoutes />;
+export default function App() {
+  return <AppRoutes />;
 }
--- drip-store/src/components/EmphasisCollection.tsx
+++ drip-store/src/components/EmphasisCollection.tsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 import Camisetas from "../assets/colections/camisetas.svg";
-import Calcas from "../assets/colections/calca.svg";
-import Headphones from "../assets/colections/headphone.svg";
+import Calcas from "../assets/colections/pants.svg";
+import Headphones from "../assets/colections/fones.svg";
 import Tenis from "../assets/colections/tenis.svg";
 
 export default function EmphasisCollection() {
-  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
   const buttons = [
-    { id: 0, image: Camisetas },
-    { id: 1, image: Calcas },
-    { id: 2, image: Calcas },
-    { id: 3, image: Headphones },
-    { id: 4, image: Tenis },
+    { id: 0, image: Camisetas, text: "Camisetas" },
+    { id: 1, image: Calcas, text: "Calças" },
+    { id: 2, image: Calcas, text: "Bonés" },
+    { id: 3, image: Headphones, text: "Headphones" },
+    { id: 4, image: Tenis, text: "Tênis" },
   ];
 
   const handleButtonClick = (index: number) => {
@@ -24,11 +24,13 @@ export default function EmphasisCollection() {
       <h1 className="flex justify-center text-2xl text-dark-gray-2 font-bold">
         Coleções em Destaque
       </h1>
-      <div className="flex flex-row gap-16 mt-16">
+      <div className="flex flex-row gap-16 mt-16 mb-40">
         {buttons.map((button) => (
-          <div className="bg-white flex items-center justify-center rounded-full p-8">
+          <div
+            key={button.id}
+            className="bg-white flex items-center justify-center rounded-full p-8 shadow-lg"
+          >
             <button
-              key={button.id}
               onClick={() => handleButtonClick(button.id)}
               style={{
                 width: "64px",
@@ -38,10 +40,13 @@ export default function EmphasisCollection() {
                 backgroundRepeat: "no-repeat",
                 filter:
                   selectedIndex === button.id
-                    ? "invert(25%) sepia(70%) saturate(1000%) hue-rotate(270deg)"
+                    ? `brightness(0) saturate(100%) invert(26%) sepia(55%) saturate(4385%) hue-rotate(315deg) brightness(89%) contrast(88%)`
                     : "invert(25%) sepia(0%) saturate(0%) hue-rotate(0deg)",
               }}
             />
+            <h3 className="absolute mt-48 font-bold text-sm text-[#474747] tracking-wider">
+              {button.text}
+            </h3>
           </div>
         ))}
       </div>
