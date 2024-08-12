49e49088594e84c7aa0309fa1e0734d2d742f1bd
Antonio880 <antoniocruzgomes880@gmail.com>
2024-08-12 17:36:23 -0300

    retirada do back para pôr em outro repositorio

--- back-drip/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { generateToken, loginUser } from '../middlewares/AuthController';
-
-const router = Router();
-
-router.post('/auth/token', generateToken);
-router.post('/auth/login', loginUser);
-
-export default router;
--- back-drip/src/types/express.d.ts
+++
@@ -1,8 +0,0 @@
-// express.d.ts
-import { JwtPayload } from 'jsonwebtoken';
-
-declare module 'express-serve-static-core' {
-  interface Request {
-    user?: string | JwtPayload; // Adicione a propriedade 'user'
-  }
-}
